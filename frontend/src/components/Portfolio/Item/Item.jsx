e481ef292f60fcea16c3aee8b0bd579efce8aff9
Marcos <marcosraimondi1@gmail.com>
2022-03-11 01:03:31 -0300

    img styles

--- frontend/src/components/Portfolio/Item/Item.jsx
+++ frontend/src/components/Portfolio/Item/Item.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function Item(props) {
   return (
     <div className="col-lg-6">
-        <a
-            className="portfolio-item"
-            href={props.href}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <div className="caption">
-                <div className="caption-content">
-                      <div className="h2">{props.caption_title}</div>
-                    <p className="mb-0">
-                        {props.caption_description}
-                    </p>
-                </div>
-            </div>
-            <img
-                className="img-fluid"
-                src={props.img_source}
-                alt="..."
-                width="700"
-                height="467"
-            />
-        </a>
+      <a
+        className="portfolio-item"
+        href={props.href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="caption">
+          <div className="caption-content">
+            <div className="h2">{props.caption_title}</div>
+            <p className="mb-0">{props.caption_description}</p>
+          </div>
+        </div>
+        <img
+          className="img-fluid"
+          style={{ maxHeight: "380 px", maxWidth: "530 px" }}
+          src={props.img_source}
+          alt="..."
+          width="700"
+          height="467"
+        />
+      </a>
     </div>
-  )
+  );
 }
