f78962a8a332a8fe166aca3111d815c422c33d94
Marcos <marcosraimondi1@gmail.com>
2022-03-11 19:22:12 -0300

    update bash script

---
+++ update.sh
@@ -0,0 +1,30 @@
+# C O N F I G U R A C I O N E S __ D E __ P R O Y E C T O (clonar repo, build front, env variables, run docker-compose)
+
+sudo docker-compose -f docker-compose-prod.yml down
+
+sudo git pull
+
+sudo rm -r ./backend/ffmpeg
+
+
+# install dependencies and build project
+
+cd frontend
+
+sudo npm install
+
+sudo npm run build
+
+cd ..
+
+sudo rm ./backend/build -r
+
+sudo mv ./frontend/build ./backend # move build to backend
+
+sudo mkdir backend/data
+
+echo -------------------------- starting docker -------------------------------------
+
+sudo docker-compose -f docker-compose-prod.yml build
+
+sudo docker-compose -f docker-compose-prod.yml up -d
