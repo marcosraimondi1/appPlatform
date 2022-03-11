49c11918f635d6347157bc464353ecee184651ff
Marcos <marcosraimondi1@gmail.com>
2022-03-11 17:06:48 -0300

    refactoring instagram

--- backend/api/helper/Instagram/instagram_scrap_follows.js
+++ backend/api/helper/Instagram/instagram_scrap_follows.js
@@ -5,97 +5,37 @@
  * 	@returns {Promise<Array<object>>}
  */
 const instagram_scrap_follows = async (page, target) => {
-  page.waitForTimeout(1500);
+  
+  await page.waitForTimeout(1500);
+
   let followers = [];
-  page.screenshot({ path: `screens/${target}1.png` });
-
-  let link_number = 2;
-
-  if (target == "following")
-    link_number = 3;
+  let link_number = target === "following" ? 3 : 2;
 
   try {
     // obtener cantidad de follows del link para abrir el popup
-    let cantidad_selector = `#react-root > section > main > div > header > section > ul > li:nth-child(${link_number}) > a > div > span`;
+    const cantidad = await getCantidad(page, link_number);
 
-    try {
-      await page.waitForSelector(cantidad_selector);
-    } catch (error) { }
-
-    await page.waitForTimeout(1000);
-
-    page.screenshot({ path: `screens/${target}2.png` });
-
-    let elemento = await page.$(cantidad_selector);
-
-    const cantidad = await page.evaluate((e) => {
-      // Open Popup
-      return parseInt(e.innerHTML);
-    }, elemento);
-
-    const link_selector = `#react-root > section > main > div > header > section > ul > li:nth-child(${link_number}) > a`;
-
-    try {
-      await page.waitForSelector(link_selector);
-      await page.click(link_selector);
-    } catch (error) { }
-
+    if (!cantidad) {
+      console.log("failed to get cantidad");
+      await page.screenshot({ path: "screens/failedToOpen" + target + ".png" });
+      return [];
+    }
     console.log("Follows: " + cantidad);
 
-    page.screenshot({ path: `screens/${target}3.png` });
-
-    const selector = "a.notranslate._0imsa > span._7UhW9.xLCgt.qyrsm.KV-D4.se6yk.T0kll"; // selector para los elementos de la lista
-
-    await page.waitForTimeout(1500);
-    page.screenshot({ path: `screens/${target}11.png` });
-
-    let items = [];
-    let ti = Date.now();
-    let prev_len = items.length;
-
-    // scrollear hasta encontrar todos los follows
-    while (Date.now() - ti < 15 * 1000) {
-      // conseguir los elementos a traves con el css selector
-      items = await page.$$(selector);
-
-      // terminar el ciclo -> todos los datos conseguidos
-      if (items.length >= cantidad)
-        break;
-
-      // si no hay cambios no actualiza el tiempo y se detendra el loop a los 15 seg
-      if (items.length === prev_len)
-        continue;
-
-      ti = Date.now();
-      prev_len = items.length;
-
-      // scrollear para cargar mas elementos
-      try {
-        await page.evaluate(() => {
-          document.getElementsByClassName("isgrP")[0].scrollTop = 9999999;
-        });
-      } catch (error) {
-        console.log(error);
-        break;
-      }
+    // open pop up
+    if (!(await openPopUp(link_number, page))) {
+      console.log("failed to open pop up");
+      await page.screenshot({ path: "screens/failedToOpen" + target + ".png" });
+      return [];
     }
+
+    const items = await scrapNames(page);
 
     // extract text from elementHandle items
     for (let i = 0; i < items.length; i++) {
       followers.push(
         await items[i].evaluate(async (node) => await node.innerHTML)
       );
-    }
-
-    // Close Pop Up
-    const close_selector = "body > div.RnEpo.Yx5HN > div > div > div > div:nth-child(1) > div > div:nth-child(3) > button";
-    try {
-      await page.click(close_selector);
-    } catch (error) {
-      // refresh page
-      await page.reload();
-      await page.waitForTimeout(1000);
-      console.log("error closing, refreshing page");
     }
 
     console.log(`--------- Scraped ${followers.length} Elements ---------`);
@@ -107,4 +47,84 @@ const instagram_scrap_follows = async (page, target) => {
   await page.waitForTimeout(1000);
   return followers;
 };
+
+/**
+ *
+ * ------------- F U N C T I O N S ---------------
+ *
+ */
+
+const scrapNames = async (page) => {
+  const selector =
+    "a.notranslate._0imsa > span._7UhW9.xLCgt.qyrsm.KV-D4.se6yk.T0kll"; // selector para los elementos de la lista
+
+  await page.waitForTimeout(1500);
+
+  let items = [];
+  let ti = Date.now();
+  let prev_len = items.length;
+
+  // scrollear hasta encontrar todos los follows
+  while (Date.now() - ti < 15 * 1000) {
+    // conseguir los elementos a traves con el css selector
+    items = await page.$$(selector);
+
+    // terminar el ciclo -> todos los datos conseguidos
+    if (items.length >= cantidad) break;
+
+    // si no hay cambios no actualiza el tiempo y se detendra el loop a los 15 seg
+    if (items.length === prev_len) continue;
+
+    ti = Date.now();
+    prev_len = items.length;
+
+    // scrollear para cargar mas elementos
+    if (!(await scroll(page))) break;
+  }
+
+  return items;
+};
+
+const scroll = async (page) => {
+  try {
+    await page.evaluate(() => {
+      document.getElementsByClassName("isgrP")[0].scrollTop = 9999999;
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const getCantidad = async (page, link_number) => {
+  try {
+    let cantidad_selector = `#react-root > section > main > div > header > section > ul > li:nth-child(${link_number}) > a > div > span`;
+
+    await page.waitForTimeout(1000);
+
+    let elemento = await page.$(cantidad_selector);
+
+    const cantidad = await page.evaluate((e) => {
+      return parseInt(e.innerHTML);
+    }, elemento);
+
+    return cantidad;
+  } catch (error) {
+    return false;
+  }
+};
+
+const openPopUp = async (link_number, page) => {
+  const link_selector = `#react-root > section > main > div > header > section > ul > li:nth-child(${link_number}) > a`;
+
+  try {
+    await page.waitForSelector(link_selector);
+    await page.click(link_selector);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 exports.instagram_scrap_follows = instagram_scrap_follows;
